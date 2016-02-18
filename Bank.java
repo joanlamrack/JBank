51936f3e1f7c776e38bc88ec447e60126d37fe69
joanlamrack <joanlamrack@gmail.com>
2016-02-19 00:05:19 +0700

    modul 1 tugas 2

---
+++ Bank.java
@@ -0,0 +1,36 @@
+
+/**
+ * Write a description of class Bank here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Bank
+{
+    // instance variables - replace the example below with your own
+    private double creditInterestRate;
+    private String closeTime;
+    private double investmentInterestRate; 
+    private int lastCustID;
+    private int nextCustID;
+    private String phone;
+    private double premiumInterestRate;
+    private String startTime;
+    public String website;
+    public String bankAddress="1234 JavaStreet, Anycity, ThisState, 34567";
+    /**
+     * Constructor for objects of class Bank
+     */
+    public Bank()
+    {
+        // initialise instance variables
+
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+}
---
+++ Teller.java
@@ -0,0 +1,30 @@
+
+/**
+ * Write a description of class Teller here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Teller
+{
+    // instance variables - replace the example below with your own
+    public static void main(String[] args){
+    
+    }
+
+    public Teller(){
+        
+    }
+    /**
+     * Constructor for objects of class Teller
+     */
+ 
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+
+}
