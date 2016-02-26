53175d61f3b5fbc72395fe1daf747b827e843b68
joanlamrack <joanalamrack@gmail.com>
2016-02-27 03:36:14 +0700

    Modul 2 Tugas 4

--- Customer.java
+++ Customer.java
@@ -1,9 +1,10 @@
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 /**
- * Write a description of class Customer here.
+ * Mewakili Kelas Kustomer yang berisi informasi tentang Kustomer serta akun
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Erithiana Sisijoan
+ * @version 2.4 (Javadoc Session & Business Rules)
  */
 public class Customer
 {
@@ -32,51 +33,122 @@ public class Customer
     public Customer(String firstName, String lastName, String dateOfBirth, int custId){
         
     }
-    
+     /**
+     * menyatukan data jalan, kota dan kode pos menjadi satu rangkaian string alamat
+     * 
+     * @return     gabungan dari string streetAddress, cityAddress dan zipOrPostalCode
+     * berurutan
+     */
     private String getAddress(){
         return streetAddress+" "+ cityAddress +" "+ zipOrPostalCode;
     }
+    /**
+     * mengambil account yang tersimpan
+     * 
+     * @return  akun yang tersimpan
+     */
     public Account getAccount(){
         return accounts;
     }
+    /**
+     * mengambil customer id yang tersimpan
+     * 
+     * @return  ID dari customer
+     */
     private int getCustomerId(){
         return custId;
     }
-    private String getEmail(){
+    /**
+     * mengambil data email
+     * 
+     * @return  nama email yang tersimpan dalam bentuk string
+     */
+    public String getEmail(){
         return email;
     }
+     /**
+     * mengambil nama kustomer dari gabungan nama depan dan belakang
+     * 
+     * @return  nama belakang kustomer, nama depan kustomer
+     */
     public String getCustomerName(){
         return lastName + " , " + firstName;
     }
+     /**
+     * menunjukan jumlah akun yang sudah dibuat
+     * 
+     * @return  jumlah akun
+     */
     public int getNumOfAccounts(){
         return numberOfCurrentAccounts;
     }
+     /**
+     * mengambil data nomor telepon
+     * 
+     * @return  nomor telepon dari kustomer
+     */
     private String getPhoneNumber(){
         return phoneNumber;
     }
+     /**
+     * membuat data alamat dengan input jalan, kota dan kode pos, dan memasukan input ke
+     * variabel yang bersangkutan
+     * 
+     * @param street alamat-nama jalan
+     * @param city alamat-nama kota
+     * @param code alamat-kode pos
+     * @return none
+     */
     private void setAddress(String street, String city, String code){
         streetAddress= street;
         cityAddress=city;
         zipOrPostalCode=code;
     }
-    private void setEmail(String emailAddress){
-        email=emailAddress;
-    }
+    /**
+     * membuat data email kustomer, dimasukkan ke variable email. Menvalidasi format
+     * tulisan email pada umumnya dengan menggunakan regex. pada terminal akan bernilai true
+     * jika penulisan sesuai.
+     * 
+     * @param emailAddress email kustomer
+     * @return none
+     */
+    public void setEmail(String emailAddress){
+        String regex="^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(emailAddress);
+        System.out.println(emailAddress +" : "+ matcher.matches());
+        if (matcher.matches()==true){
+            email=emailAddress;
+        }
+}
+    /**
+     * membuat data nama kustomer, dimasukkan ke dua variabel untuk memisahkan
+     * nama depan dan belakang
+     * 
+     * @param lname nama keluarga kustomer
+     * @param fname nama depan kustomer
+     * @return none
+     */
     public void setName(String lname, String fname){
         lastName=lname;
         firstName=fname;
     }
+     /**
+     * untuk memasukkan nomor telepon baru, phoneNum dimasukkan dalam string phoneNumber
+     * 
+     * @param  phoneNum   nomor telepon dalam bentuk string ""
+     * @return  none
+     */
     private void setPhoneNumber(String phoneNum){
         phoneNumber=phoneNum;
     }
+    /**
+     * To set an account
+     * 
+     * @param  akun nama akun yang akan ditambah
+     * @return none
+     */
     public void setAccount(Account akun){
         accounts=akun;
     }
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-
 }
